03753bf4d690f786a7c1ef9da5e09e7b5ed54ba8
NuttakitKongkaew <68010334@kmitl.ac.th>
2026-01-21 16:53:23 +0700

    68010334

--- src/practicals/p07/persons.ts
+++ src/practicals/p07/persons.ts
@@ -4,8 +4,28 @@ interface Person {
   age?: number;
 }
 
-function sortPersons(persons: Person[]): Person[] {}
+export function sortPersons(persons: Person[]): Person[] {
+  return persons.sort((a, b) => {
 
+    const ageA = a.age ?? Infinity;
+    const ageB = b.age ?? Infinity;
+
+    if (ageA !== ageB) {
+        return ageA - ageB;
+    }
+
+
+    const firstNameA = a.firstName?.toLowerCase() || "";
+    const firstNameB = b.firstName?.toLowerCase() || "";
+    if (firstNameA !== firstNameB) {
+        return firstNameA.localeCompare(firstNameB);
+    }
+
+    const lastNameA = a.lastName?.toLowerCase() || "";
+    const lastNameB = b.lastName?.toLowerCase() || "";
+    return lastNameA.localeCompare(lastNameB);
+});
+}
 
 const persons: Person[] = [
   { firstName: "Somchai", lastName: "Jaidee", age: 30 },
@@ -16,3 +36,6 @@ const persons: Person[] = [
   { firstName: "Jane", lastName: "Smith", age: -5 },
   { lastName: "NoName", age: 40 }
 ];
+
+const sortedPersons = sortPersons(persons);
+console.log(sortedPersons);
--- src/practicals/p08/albums.ts
+++ src/practicals/p08/albums.ts
@@ -3,4 +3,6 @@ interface FullAlbum {}
 const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos';
 const ALBUMS_URL = 'https://jsonplaceholder.typicode.com/albums';
 
-function mapPhotoToAlbum(userIds?: number[]): Promise<FullAlbum[]>
+function mapPhotoToAlbum(userIds?: number[]): Promise<FullAlbum[]>{
+    for
+}
