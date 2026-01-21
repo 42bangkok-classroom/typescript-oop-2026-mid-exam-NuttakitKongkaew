206c74c1f12db46b6dad119b5481f65ab68a1859
NuttakitKongkaew <68010334@kmitl.ac.th>
2026-01-21 16:58:47 +0700

    68010334

--- src/practicals/p09/read-json.ts
+++ src/practicals/p09/read-json.ts
@@ -1,11 +1,10 @@
-// import * as fs from "fs";
+import * as fs from "fs";
 
-// function readJson(fileName: string): unknown[] {
-//     const rawData = fs.readFileSync(fileName, "utf-8");
-//     const product = JSON.parse(rawData);
-//     return product;
-// }
+function readJson(fileName: string): unknown[] {
+    const rawData = fs.readFileSync(fileName, "utf-8");
+    const product = JSON.parse(rawData);
+    return product;
+}
 
-// const products = readJson("product.json");
-// console.log(products);
-
+const products = readJson("product.json");
+console.log(products);
