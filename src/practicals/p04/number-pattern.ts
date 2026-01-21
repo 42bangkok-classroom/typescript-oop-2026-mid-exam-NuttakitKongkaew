dd502ac62ca778240c476baf5bc0f1af01309941
NuttakitKongkaew <68010334@kmitl.ac.th>
2026-01-21 15:00:51 +0700

    68010334

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,13 @@
 // Write your code below
+export{}
+const input = process.argv[2];
+const size = Number(input);
+
+for(let i =1 ; i<size-1 ; i++ ) {
+  let row = ""
+  for(let j =0 ; j<size ; j++ ){
+    row += i
+  }
+  console.log(row);
+}
+// j=4 i=1
