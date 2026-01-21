1dbc4f194427721d05747a2f04a1599c6ee62053
NuttakitKongkaew <68010334@kmitl.ac.th>
2026-01-21 15:06:56 +0700

    68010334

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,20 @@
 // Write your code below
+export{};
+let input = process.argv.slice(2);
+let n = Number(input[0]);
+let raw = input[0];
+if(raw == "" || raw == " "){
+    console.log("Invalid input");
+    process.exit();
+}
+for(let i = 1;i <= n ;i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");
+    }else if (i % 3 === 0){
+        console.log("Fizz");
+    }else if (i % 5 === 0){
+        console.log("Buzz");
+    }else{
+        console.log(String(i));
+    }
+}
