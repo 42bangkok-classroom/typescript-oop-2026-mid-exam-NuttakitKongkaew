6ef62ae24145e84e069f23cd6a44f1265ff0db3f
NuttakitKongkaew <68010334@kmitl.ac.th>
2026-01-21 16:44:41 +0700

    68010334

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,8 +1,24 @@
 export function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
   // Write your code below
   const combinedArray = [...arr1, ...arr2];
-  const uniqueNumbers = combinedArray.filter((item, index) => combinedArray.indexOf(item) === index);
-  return uniqueNumbers;
+  const uniqueNumbers: number[] = [];
+  for (let i = 0; i < combinedArray.length; i++) {
+    const current = combinedArray[i];
+
+    let count = 0;
+    for (let j = 0; j < combinedArray.length; j++) {
+        if (combinedArray[j] === current) {
+            count++;
+        }
+    }
+
+
+    if (count === 1) {
+        uniqueNumbers.push(current);
+    }
+}
+
+return uniqueNumbers;
 }
 
 const arr1 = [1, 2, 3, 4];
@@ -10,6 +26,10 @@ const arr2 = [3, 4, 5, 6];
 
 const uniqueNumbers = getUniqueNumbers(arr1, arr2);
 console.log(uniqueNumbers);
+
+
+
+
 
 
 
